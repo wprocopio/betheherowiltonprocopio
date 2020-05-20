3c5ed414b94c9239cd79d5942a737bc0c45ebfbd
wilton procopio <wprocopio@gmail.com>
2020-05-20 15:15:47 -0300

    colocar log para debug

--- src/controllers/IncidentsController.js
+++ src/controllers/IncidentsController.js
@@ -31,7 +31,7 @@ module.exports = {
         // gerando o ID randomico
         const ong_id = req.headers.authorization;
 
-        //console.log(title, description, valor, ong_id);
+        console.log(title, description, valor, ong_id);
 
         const reesult = await conection('incidents').insert({
             title, description, valor, ong_id,
