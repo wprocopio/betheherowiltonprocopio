ea4eea52cb2787b2928127006dda2d6e253efa29
wilton procopio <wprocopio@gmail.com>
2020-05-19 23:51:29 -0300

    ajuste para orgin mudando a linha da sequencia do cors 3

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 app.use(cors());
 app.use(express.json());
