9b2d4aa26f47ca3411e6e181172dfc61872bf3f7
wilton procopio <wprocopio@gmail.com>
2020-05-31 17:47:22 -0300

    ajustes no controller indexjs raiz

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+  origin: 'https://serene-hugle-efa406.netlify.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
+
+
 app.use(express.json());
 app.use(routes);
+
+
 
 const PORT = process.env.PORT;
 
